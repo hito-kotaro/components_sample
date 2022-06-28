3252be68e1babb184d1f2478f049e65610a5cae2
hito-kotaro <tohi.work@gmail.com>
2022-06-29 00:04:27 +0900

    dropdownListの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
+import { IconClipboard, IconCopy, IconTrash } from '@supabase/ui';
 import MyWorkMenuList from './components/Lists/MyWorkMenuList';
+import DropDown from './components/DropDown/DropDown';
+import { DropDownItem } from './types/DropDownItemType';
 
 const App = () => {
+  const onClick = () => {
+    console.log('test');
+  };
+
+  const itemList: DropDownItem[] = [
+    { icon: <IconClipboard />, onClick, text: 'Duplicate', divider: false },
+    { icon: <IconCopy />, onClick, text: 'Copy', divider: false },
+    { icon: <IconTrash />, onClick, text: 'Trash', divider: false },
+    { icon: <IconClipboard />, onClick, text: 'TEST!!', divider: true },
+  ];
   return (
     <div>
-      <h1 className="text-2xl">IconLabelButton</h1>
-
+      <h1 className="text-2xl">IconLabelButton & List Frame</h1>
       <div className="flex justify-center">
         <div className="w-11/12">
           <MyWorkMenuList />
         </div>
+      </div>
+
+      <h1 className="text-2xl">DropDownMenuButton</h1>
+      <div className="ml-auto bg-red-50 text-right pr-5">
+        <DropDown itemList={itemList} />
       </div>
     </div>
   );
---
+++ src/components/DropDown/DropDown.tsx
@@ -0,0 +1,34 @@
+import React, { ReactElement, VFC } from 'react';
+import { BsThreeDots } from 'react-icons/bs';
+import { Button, Divider, Dropdown, Typography } from '@supabase/ui';
+import { DropDownItem } from '../../types/DropDownItemType';
+
+type Props = {
+  itemList: DropDownItem[];
+};
+const DropDown: VFC<Props> = (props) => {
+  const { itemList } = props;
+  const onClick = () => {
+    console.log('test');
+  };
+
+  return (
+    <Dropdown
+      overlay={itemList.map((item: DropDownItem) => (
+        <>
+          {item.divider ? <Divider light /> : ''}
+          <Dropdown.Item icon={item.icon} onClick={item.onClick}>
+            <Typography.Text>{item.text}</Typography.Text>
+          </Dropdown.Item>
+        </>
+      ))}
+    >
+      <Button
+        type="text"
+        icon={<BsThreeDots size={24} className=" forcus:outline-none" />}
+      />
+    </Dropdown>
+  );
+};
+
+export default DropDown;
